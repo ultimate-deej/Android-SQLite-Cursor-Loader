f95845155b674682bfad1ef76b0950e0ff06c0e6
Maxim Naumov <ultimate.deej@gmail.com>
2014-02-13 20:29:53 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Android SQLite Cursor Loader
+============================
+This is like the framework's `CursorLoader` but for SQLite. Content changes not detected! Do it yourself `yourLoader.onContentChanged()` after insert/delete/update.
+
+License
+=======
+
+    Copyright 2014 Maxim Naumov
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ lib/src/main/java/org/deejdev/database/sqlitecursorloader/support/SQLiteCursorLoader.java
@@ -0,0 +1,156 @@
+package org.deejdev.database.sqlitecursorloader.support;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v4.content.AsyncTaskLoader;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class SQLiteCursorLoader extends AsyncTaskLoader<Cursor> {
+    private final SQLiteOpenHelper sqLiteOpenHelper;
+    private final boolean distinct;
+    private final String table;
+    private final String[] columns;
+    private final String selection;
+    private final String[] selectionArgs;
+    private final String groupBy;
+    private final String having;
+    private final String orderBy;
+    private final String limit;
+    private Cursor cursor;
+
+    public SQLiteCursorLoader(Context context, SQLiteOpenHelper sqLiteOpenHelper, boolean distinct,
+                              String table, String[] columns, String selection,
+                              String[] selectionArgs, String groupBy, String having,
+                              String orderBy, String limit) {
+        super(context);
+        this.sqLiteOpenHelper = sqLiteOpenHelper;
+        this.distinct = distinct;
+        this.table = table;
+        this.columns = columns;
+        this.selection = selection;
+        this.selectionArgs = selectionArgs;
+        this.groupBy = groupBy;
+        this.having = having;
+        this.orderBy = orderBy;
+        this.limit = limit;
+    }
+
+    /* Runs on a worker thread */
+    @Override
+    public Cursor loadInBackground() {
+        Cursor cursor = sqLiteOpenHelper.getReadableDatabase()
+                .query(distinct, table, columns, selection, selectionArgs, groupBy, having, orderBy, limit);
+        if (cursor != null) {
+            // Ensure the cursor window is filled
+            cursor.getCount();
+        }
+        return cursor;
+    }
+
+    /* Runs on the UI thread */
+    @Override
+    public void deliverResult(Cursor cursor) {
+        if (isReset()) {
+            // An async query came in while the loader is stopped
+            if (cursor != null) {
+                cursor.close();
+            }
+            return;
+        }
+        Cursor oldCursor = this.cursor;
+        this.cursor = cursor;
+
+        if (isStarted()) {
+            super.deliverResult(cursor);
+        }
+
+        if (oldCursor != null && oldCursor != cursor && !oldCursor.isClosed()) {
+            oldCursor.close();
+        }
+    }
+
+    /**
+     * Starts an asynchronous load of the list data. When the result is ready the callbacks
+     * will be called on the UI thread. If a previous load has been completed and is still valid
+     * the result may be passed to the callbacks immediately.
+     * <p/>
+     * Must be called from the UI thread
+     */
+    @Override
+    protected void onStartLoading() {
+        if (cursor != null) {
+            deliverResult(cursor);
+        }
+        if (takeContentChanged() || cursor == null) {
+            forceLoad();
+        }
+    }
+
+    /**
+     * Must be called from the UI thread
+     */
+    @Override
+    protected void onStopLoading() {
+        // Attempt to cancel the current load task if possible.
+        cancelLoad();
+    }
+
+    @Override
+    public void onCanceled(Cursor cursor) {
+        if (cursor != null && !cursor.isClosed()) {
+            cursor.close();
+        }
+    }
+
+    @Override
+    protected void onReset() {
+        super.onReset();
+
+        // Ensure the loader is stopped
+        onStopLoading();
+
+        if (cursor != null && !cursor.isClosed()) {
+            cursor.close();
+        }
+        cursor = null;
+    }
+
+    @Override
+    public void dump(String prefix, FileDescriptor fd, PrintWriter writer, String[] args) {
+        super.dump(prefix, fd, writer, args);
+        writer.print(prefix);
+        writer.print("distinct=");
+        writer.println(distinct);
+        writer.print(prefix);
+        writer.print("table=");
+        writer.println(table);
+        writer.print(prefix);
+        writer.print("columns=");
+        writer.println(Arrays.toString(columns));
+        writer.print(prefix);
+        writer.print("selection=");
+        writer.println(selection);
+        writer.print(prefix);
+        writer.print("selectionArgs=");
+        writer.println(Arrays.toString(selectionArgs));
+        writer.print(prefix);
+        writer.print("groupBy=");
+        writer.println(groupBy);
+        writer.print(prefix);
+        writer.print("having=");
+        writer.println(having);
+        writer.print(prefix);
+        writer.print("orderBy=");
+        writer.println(orderBy);
+        writer.print(prefix);
+        writer.print("limit=");
+        writer.println(limit);
+        writer.print(prefix);
+        writer.print("cursor=");
+        writer.println(cursor);
+    }
+}
